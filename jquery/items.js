095d809e6bb072b4c879f15d7881739e4196e6e8
Youssef02222 <93304532+Youssef02222@users.noreply.github.com>
2022-04-05 02:35:02 +0200

    Add files via upload

---
+++ jquery/items.js
@@ -0,0 +1,16 @@
+const items = [{
+    name: "water",
+    price: 10,
+    imagsrc: "water.jpg"
+},
+{
+    name: "Mango",
+    price: 50,
+    imagsrc: "mango-juice.jpg"
+},
+{
+    name: "coffe",
+    price: 35,
+    imagsrc: "coffee.jpg"
+}
+]
